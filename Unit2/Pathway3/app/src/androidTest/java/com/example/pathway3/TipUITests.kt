8c7d49a6f4896a34c9918341876d8b0b15266192
Разиль <razilchick@mail.ru>
2026-09-20 21:52:21 +0300

    Pathway 3: Add local unit test and calculator

---
+++ Unit2/Pathway3/app/src/androidTest/java/com/example/pathway3/TipUITests.kt
@@ -0,0 +1,28 @@
+package com.example.pathway3
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.example.pathway3.ui.theme.Pathway3Theme
+import org.junit.Rule
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            Pathway3Theme {
+                TipTimeLayout()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage").performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists()
+    }
+}
